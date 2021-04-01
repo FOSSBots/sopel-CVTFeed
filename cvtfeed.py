96c136a8576ac8bcf7bd2c57ee7bed9fe2ce572d
MacFan4000 <paulfkeffer@gmail.com>
2021-04-01 09:37:39 -0400

    more fixes

--- cvtfeed.py
+++ cvtfeed.py
@@ -1,14 +1,11 @@
-"""
-cvtfeed.py - Miraheze CVT Feed Plugin
-"""
-
-
-from sopel.config.types import ListAttribute, StaticSection, ValidatedAttribute
-from sopel import plugin
-from sopel import tools
-from sopel.module import commands, example, require_admin, require_chanmsg, rule
+"""cvtfeed.py - Miraheze CVT Feed Plugin."""
 
 import re
+
+from sopel import plugin
+from sopel.config.types import ListAttribute, StaticSection, ValidatedAttribute
+from sopel import tools
+from sopel.module import commands, example, require_admin, require_chanmsg, rule
 import sys
 
 if sys.version_info.major >= 3:
@@ -34,7 +31,7 @@ def configure(config):
     config.define_section('cvtfeed', CVTFeedSection, validate=False)
     config.cvtfeed.configure_setting('stringpatterns', 'Enter strings to look for in feed items.')
     config.cvtfeed.configure_setting('regexpatterns', 'Enter regex patterns to look for in feed items')
-    config.cvtfeed.configure_setting('destination_channels', "Enter channels where matching feed items should be sent.")
+    config.cvtfeed.configure_setting('destination_channels', 'Enter channels where matching feed items should be sent.')
     config.cvtfeed.configure_setting('feed_account', 'Enter the NickServ account name of the bot that posts feed items in the feed channel.')
     config.cvtfeed.configure_setting('feed_channel', 'Enter the feed channel name.')
 
@@ -64,13 +61,13 @@ def match_items(bot, trigger):
 def manage_pattern(bot, trigger):
     """Manage cvt feed patterns."""
     STRINGS = {
-        "success_del": "Successfully deleted pattern: %s",
-        "success_add": "Successfully added pattern: %s",
-        "no_string": "No matching string pattern found for: %s",
-        "no_regex": "No matching regex pattern found for: %s",
-        "invalid": "Invalid format for %s a pattern. Try: .cvtpattern add (string|regex) sopel",
-        "invalid_display": "Invalid input for displaying patterns.",
-        "nonelisted": "No %s listed in the patterns list.",
+        'success_del': 'Successfully deleted pattern: %s',
+        'success_add': 'Successfully added pattern: %s',
+        'no_string': 'No matching string pattern found for: %s',
+        'no_regex': 'No matching regex pattern found for: %s',
+        'invalid': 'Invalid format for %s a pattern. Try: .cvtpattern add (string|regex) sopel',
+        'invalid_display': 'Invalid input for displaying patterns.',
+        'nonelisted': 'No %s listed in the patterns list.',
         'huh': "I could not figure out what you wanted to do.",
     }
 
@@ -80,14 +77,14 @@ def manage_pattern(bot, trigger):
                   for regex in bot.config.cvtfeed.regexpatterns if regex != '')
     text = trigger.group().split()
 
-    if len(text) == 3 and text[1] == "list":
-        if text[2] == "string":
+    if len(text) == 3 and text[1] == 'list':
+        if text[2] == 'string':
             if len(strings) > 0:
                 patterns = ', '.join(unicode(string) for string in strings)
                 bot.say(f'Patterns: {patterns}')
             else:
                 bot.reply(STRINGS['nonelisted'] % ('strings'))
-        elif text[2] == "regex":
+        elif text[2] == 'regex':
             if len(regexes) > 0:
                 patterns = ', '.join(unicode(regex) for regex in regexes)
                 bot.say(f'Patterns: {patterns}')
@@ -96,23 +93,23 @@ def manage_pattern(bot, trigger):
         else:
             bot.reply(STRINGS['invalid_display'])
 
-    elif len(text) == 4 and text[1] == "add":
-        if text[2] == "regex":
+    elif len(text) == 4 and text[1] == 'add':
+        if text[2] == 'regex':
             regexes.add(text[3])
             bot.config.cvtfeed.regexpatterns = regexes
             bot.config.save()
-        elif text[2] == "string":
+        elif text[2] == 'string':
             strings.add(text[3])
             bot.config.cvtfeed.stringpatterns = strings
             bot.config.save()
         else:
-            bot.reply(STRINGS['invalid'] % ("adding"))
+            bot.reply(STRINGS['invalid'] % ('adding'))
             return
 
         bot.reply(STRINGS['success_add'] % (text[3]))
 
-    elif len(text) == 4 and text[1] == "del":
-        if text[2] == "regex":
+    elif len(text) == 4 and text[1] == 'del':
+        if text[2] == 'regex':
             if tools.Identifier(text[3]) not in regexes:
                 bot.reply(STRINGS['no_regex'] % (text[3]))
                 return
@@ -120,7 +117,7 @@ def manage_pattern(bot, trigger):
             bot.config.cvtfeed.regexpatterns = [unicode(r) for r in regexes]
             bot.config.save()
             bot.reply(STRINGS['success_del'] % (text[3]))
-        elif text[2] == "string":
+        elif text[2] == 'string':
             if tools.Identifier(text[3]) not in strings:
                 bot.reply(STRINGS['no_string'] % (text[3]))
                 return
@@ -129,7 +126,7 @@ def manage_pattern(bot, trigger):
             bot.config.save()
             bot.reply(STRINGS['success_del'] % (text[3]))
         else:
-            bot.reply(STRINGS['invalid'] % ("deleting"))
+            bot.reply(STRINGS['invalid'] % ('deleting'))
             return
     else:
         bot.reply(STRINGS['huh'])
@@ -143,15 +140,15 @@ def manage_channel(bot, trigger):
     text = trigger.group().split()
     if text[1] == "on":
         if trigger.sender in bot.config.cvtfeed.destination_channels:
-            bot.say("The feed is already enabled for this channel")
+            bot.say('The feed is already enabled for this channel')
         else:
             bot.config.cvtfeed.destination_channels.add(trigger.sender)
             bot.config.save()
-            bot.say("Successfully enabled the feed for this channel")
+            bot.say('Successfully enabled the feed for this channel')
     elif text[1] == "off":
         if trigger.sender not in bot.config.cvtfeed.destination_channels:
-            bot.say("The feed is already disabled for this channel")
+            bot.say('The feed is already disabled for this channel')
         else:
             bot.config.cvtfeed.destination_channels.remove(trigger.sender)
             bot.config.save()
-            bot.say("Successfully disabled the feed for this channel")
+            bot.say('Successfully disabled the feed for this channel')
